58a555c0893a1c45c43e7b842dec25313df95816
Trentist <chasegreer327@gmail.com>
2023-11-07 12:07:30 +0600

    fix: update create2 factory call external functions

--- contracts/Create2Factory.sol
+++ contracts/Create2Factory.sol
@@ -4,14 +4,22 @@
 
 import "@openzeppelin/contracts/utils/Create2.sol";
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+import {Address} from "@openzeppelin/contracts/utils/Address.sol";
 
 pragma solidity 0.8.17;
 
 /// @title Create2Factory
 /// @notice Deploys contract from bytecode and salt using create2
 contract Create2Factory is Ownable {
+    using Address for address;
+
     function callExternal(address target, bytes calldata data) external onlyOwner {
-        (bool success,) = target.call(data);
+        target.functionCall(data, "Call to external contract failed");
+        require(success, "External call failed");
+    }
+
+    function callExternalWithValue(address target, bytes calldata data, uint256 value) external onlyOwner {
+        target.functionCallWithValue(data, value, "Call to external contract failed");
         require(success, "External call failed");
     }
 
